08a5d9e2a4ccc84853ceb045b4651104de387328
alexeylitvinov <alextwin1976@gmail.com>
2023-12-20 17:37:26 +0100

    add func title

--- str_func.py
+++ str_func.py
@@ -1,2 +1,12 @@
 def get_upper(st: str):
+    """
+    Возвращает слово большими буквами
+    """
     return st.upper()
+
+
+def get_title(st: str):
+    """
+    Возвращает слово с заглавной буквы
+    """
+    return st.title()
